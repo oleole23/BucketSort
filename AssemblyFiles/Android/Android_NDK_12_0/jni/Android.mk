f6a37ec89c04abe5239197a981c951d37f85e891
oleole23 <olesyas2303@mail.ru>
2024-03-03 23:32:04 +0300

    commit

---
+++ AssemblyFiles/Android/Android_NDK_12_0/jni/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH:= $(call my-dir)
+
+#
+# BucketSort
+#
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../../Eco.Core1/SharedFiles
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../Eco.InterfaceBus1/SharedFiles
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../Eco.MemoryManager1/SharedFiles
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../HeaderFiles
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../SharedFiles
+TARGET_PLATFORM := android-9
+TARGET_OUT := $(LOCAL_PATH)/../../../../BuildFiles/Android/$(TARGET_ARCH_ABI)/$(CONFIGURATION)
+LOCAL_SRC_FILES := ../../../../SourceFiles/CBucketSort.c \
+../../../../SourceFiles/CBucketSortFactory.c
+
+ifeq ($(TARGET_LINK),0)
+
+LOCAL_MODULE := lib6343D68AB28A4F8FABE90F23A040FC88
+LOCAL_CFLAGS := -std=c99 -DECO_LIB -DECO_ANDROID -DUGUID_UTILITY -D__ANDROID__
+
+include $(BUILD_STATIC_LIBRARY)
+
+else
+
+LOCAL_MODULE := 6343D68AB28A4F8FABE90F23A040FC88
+LOCAL_CFLAGS := -std=c99 -DECO_DLL -DECO_ANDROID -DUGUID_UTILITY -D__ANDROID__
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
